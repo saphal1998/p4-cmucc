2f534f54ba59120764bed608ebec0e44a82ca588
saphalp <saphal1998@gmail.com>
2023-03-26 17:09:20 -0400

    WIP: Modified run.sh

--- run.sh
+++ run.sh
@@ -2,6 +2,6 @@
 ###### TEMPLATE run.sh ######
 ###### YOU NEED TO UNCOMMENT THE FOLLOWING LINE AND INSERT YOUR OWN PARAMETERS ######
 
-spark-submit --conf spark.driver.memory=5g --conf spark.default.parallelism=80 --num-executors=5 --executor-cores=4 --class PageRank target/project_spark.jar wasb://datasets@clouddeveloper.blob.core.windows.net/iterative-processing/Graph wasbs:///pagerank-output
+spark-submit --conf spark.driver.memory=12g --conf spark.default.parallelism=40 --conf spark.executor.cores=4 --conf spark.executor.instances=10 --class PageRank target/project_spark.jar wasb://datasets@clouddeveloper.blob.core.windows.net/iterative-processing/Graph wasbs:///pagerank-output
 
 # For more information about tuning the Spark configurations, refer: https://spark.apache.org/docs/2.3.0/configuration.html
